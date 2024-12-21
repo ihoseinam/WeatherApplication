1ecd4b1a4ebbb01337d9044ce8611b1e2d5c27aa
Hosein Ahmadi <iamohoseinam@gmail.com>
2024-12-21 17:09:48 +0330

    add NetWork and ViewModel module

---
+++ app/src/main/java/ir/hoseinahmadi/weatherapplication/di/NetworkModule.kt
@@ -0,0 +1,24 @@
+package ir.hoseinahmadi.weatherapplication.di
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.android.Android
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import ir.hoseinahmadi.weatherapplication.MainRepository
+import ir.hoseinahmadi.weatherapplication.viewModel.MainViewModel
+import kotlinx.serialization.json.Json
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+
+val viewModelModule = module {
+    viewModel<MainViewModel> { MainViewModel(get()) }
+}
+val NetworkModule = module {
+    single {
+        HttpClient(Android) {
+            install(ContentNegotiation) { json(Json { this.ignoreUnknownKeys = true }) }
+        }
+    }
+    single { MainRepository(get()) }
+}
