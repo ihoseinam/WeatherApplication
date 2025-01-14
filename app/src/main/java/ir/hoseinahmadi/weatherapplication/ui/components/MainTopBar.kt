478ec64b4afa39dd9ae76155d563492f59d5e276
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-01-14 15:39:48 +0330

    update onClick support icon

--- app/src/main/java/ir/hoseinahmadi/weatherapplication/ui/components/MainTopBar.kt
+++ app/src/main/java/ir/hoseinahmadi/weatherapplication/ui/components/MainTopBar.kt
@@ -1,9 +1,11 @@
 package ir.hoseinahmadi.weatherapplication.ui.components
 
+import android.widget.Toast
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
@@ -14,7 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalUriHandler
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import ir.hoseinahmadi.weatherapplication.R
 import ir.hoseinahmadi.weatherapplication.viewModel.MainViewModel
 
 @Composable
@@ -22,6 +28,9 @@ fun MainTopBar(
     mainViewModel: MainViewModel,
     color: Color,
 ) {
+    val uriHandler = LocalUriHandler.current
+    val context = LocalContext.current
+
     Row(
         modifier = Modifier
             .statusBarsPadding()
@@ -30,18 +39,26 @@ fun MainTopBar(
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        IconButton(onClick = {}) {
+        IconButton(onClick = {
+            try {
+                uriHandler.openUri("tg://resolve?domain=i_hoseinam")
+            } catch (e: Exception) {
+                Toast.makeText(context, "تلگرام یافت نشد", Toast.LENGTH_SHORT).show()
+            }
+        }) {
             Icon(
-                imageVector = Icons.Rounded.MoreVert,
+                painter = painterResource(R.drawable.support),
                 contentDescription = "",
-                tint = color
+                tint = color,
+                modifier = Modifier.size(20.dp)
             )
         }
         IconButton(onClick = { mainViewModel.updateSheetAddCityState(true) }) {
             Icon(
                 imageVector = Icons.Rounded.Add,
                 contentDescription = "",
-                tint = color
+                tint = color,
+                modifier = Modifier.size(25.dp)
             )
 
         }
